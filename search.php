e8ca6c40e878e55a37f21f6bbcef499201063525
root <root@studvm73-p.cs.ucl.ac.uk>
2013-10-28 22:52:07 +0000

    10th time trying to add the searc; I think I've got it

--- search.php
+++ search.php
@@ -56,7 +56,7 @@
     }
 
     // Retrieve data
-    $sql_select = "SELECT * FROM registration_tbl WHERE name='{$name}' email='{$email}' date='{$date}' company='{$company}'";
+    $sql_select = "SELECT * FROM registration_tbl WHERE name='{$name}'OR email='{$email}' OR date='{$date}' OR company='{$company}'";
     $stmt = $conn->query($sql_select);
     $registrants = $stmt->fetchAll();
     if(count($registrants) > 0) {
